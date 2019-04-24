34eff863e5530186ca41efc24859806286a8b3cb
mirzafurqaan <48507334+mirzafurqaan@users.noreply.github.com>
2019-04-24 18:31:45 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-    module.exports = { mongoURI: 'mongodb+srv://root:Myproject@123@cluster0-4inkv.mongodb.net/test?retryWrites=true' }
+    module.exports = { mongoURI: 'mongodb+srv://root:admin@cluster0-4inkv.mongodb.net/test?retryWrites=true' }
 } else {
     module.exports = { mongoURI: 'mongodb://localhost/vidjot-dev' }
 }
